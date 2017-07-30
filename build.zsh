a8434fd7aa83b602621a1cdc5a37484c3134aeeb
RyanSquared <vandor2012@gmail.com>
2017-07-30 00:17:47 -0500

    build.zsh: Update build script

--- build.zsh
+++ build.zsh
@@ -1,8 +1,10 @@
-#!/usr/bin/zsh
+#!/usr/bin/zsh -e
 
 alias stat='stat -c %Y'
-for file in *.moon; do
-	if ! grep "^spec" <<<$file; then
+for file in $(find . -type f -name '*.moon' | grep -v '_spec'); do
+	if ! [ -e ${file%.moon}.lua ]; then
+		moonc $file
+	elif ! grep "^spec" <<<$file; then
 		[ $(stat $file) -gt $(stat ${file%.moon}.lua) ] && moonc $file
 	fi
 done
